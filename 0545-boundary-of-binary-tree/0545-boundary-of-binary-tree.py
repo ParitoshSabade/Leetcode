6f483febb4e7c64d7a108ef424bc681052e2e880
Paritosh Sabade <77101080+ParitoshSabade@users.noreply.github.com>
2024-06-21 18:20:53 -0500

    Time: 37 ms (84.75%), Space: 17.7 MB (16.83%) - LeetHub

---
+++ 0545-boundary-of-binary-tree/0545-boundary-of-binary-tree.py
@@ -0,0 +1,75 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def boundaryOfBinaryTree(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        elif root.left == None and root.right == None:
+            return [root.val]
+        
+        answer = [root.val]
+        
+        
+        def findLeftBoundary(node):
+            if not node or (not node.left and not node.right):
+                return
+            answer.append(node.val)
+            if node.left:
+                findLeftBoundary(node.left)
+            else:
+                findLeftBoundary(node.right)
+        
+        
+        def findRightBoundary(node):
+            if not node or (not node.left and not node.right):
+                return
+            if node.right:
+                findRightBoundary(node.right)
+            else:
+                findRightBoundary(node.left)
+            answer.append(node.val)
+        
+       
+        def findLeaves(node):
+            if not node:
+                return
+            if not node.left and not node.right:
+                leaves.append(node.val)
+                return
+            findLeaves(node.left)
+            findLeaves(node.right)
+        
+        
+        if root.left:
+            findLeftBoundary(root.left)
+        
+        
+        leaves = []
+        findLeaves(root)
+        answer.extend(leaves)
+        
+        
+        rightBoundary = []
+        if root.right:
+            findRightBoundary(root.right)
+        answer.extend(reversed(rightBoundary))
+        
+        return answer
+
+    
+    
+        
+            
+            
+            
+                
+            
+                
+            
+            
+        
+        
