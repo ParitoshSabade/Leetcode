9771ead36dafebd8e4c010c76b8a84d277421632
Paritosh Sabade <77101080+ParitoshSabade@users.noreply.github.com>
2024-08-18 20:56:04 -0500

    Time: 96 ms (21.27%), Space: 21.2 MB (63.64%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,17 +2,8 @@ import heapq as hq
 from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        freq = [[] for i in range(len(nums) + 1)]
-
-        for n in nums:
-            count[n] = 1 + count.get(n, 0)
-        for n, c in count.items():
-            freq[c].append(n)
-
-        res = []
-        for i in range(len(freq) - 1, 0, -1):
-            for n in freq[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
+        num_count = Counter(nums)
+        solution = hq.nlargest(k,num_count.items(),key = lambda x:x[1])
+        solution = [k for k,v in solution]
+        print(solution)
+        return solution
