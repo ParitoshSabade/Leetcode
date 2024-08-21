54f7bff1cdb67261263db7bc50dbb3074d2ad173
Paritosh Sabade <77101080+ParitoshSabade@users.noreply.github.com>
2024-08-20 19:22:51 -0500

    Time: 42 ms (11.34%), Space: 16.4 MB (77.79%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,26 +1,26 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if digits == "" or digits == "1":
-            return []
-        phone_map = {"2": ["a","b","c"],"3": ["d","e","f"],"4": ["g","h","i"],"5": ["j","k","l"],"6": ["m","n","o"],"7": ["p","q","r",'s'],"8": ["t","u","v"],"9": ["w","x","y","z"]}
-        
-        digitsList = list(digits)
-        tmp1 = phone_map[digitsList[0]]
-        
-        
-        for i in range(1,len(digitsList)):
-            tmp2 = []
-            for t in tmp1:
-                for k in phone_map[digitsList[i]]:
-                    tmp2.append(t+k)
-                    
-            tmp1 = tmp2
-            
-            
-        return tmp1
-            
-        
-        
-        
-        
+        phone_map = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+
+        res = []
+
+        def backtrack(i,currstr):
+            if len(currstr) == len(digits):
+                res.append(currstr)
+                return
+
+            for c in phone_map[digits[i]]:
+                backtrack(i+1,currstr+c)
+        if digits:
+            backtrack(0,"")
+        return res
         
